7c542bd183c9d0e05ec8d8730070ff120ae88d72
FlameKat53 <38171335+FlameKat53@users.noreply.github.com>
2019-04-30 14:56:57 -0700

    Revert "Revert "Update driveMenu.cpp""
    This reverts commit 6f10f3cf3a7fc9cc6620af472d2a65bc85ac43e7.

--- menu/arm9/source/driveMenu.cpp
+++ menu/arm9/source/driveMenu.cpp
@@ -39,8 +39,6 @@
 
 using namespace std;
 
-//static bool ramDumped = false;
-
 bool flashcardMountSkipped = true;
 static bool flashcardMountRan = true;
 static bool dmTextPrinted = false;
@@ -67,7 +65,7 @@ void loadBootNds(void) {
 	irqDisable(IRQ_VBLANK);
 	vramSetBankA(VRAM_A_MAIN_BG);
 	vramSetBankB(VRAM_B_MAIN_BG);
-	// Clear VRAM A and B to show black border for GBA mode
+	// Clear VRAM A and B
 	for (u32 i = 0; i < 0x80000; i++) {
 		*(u32*)(0x06000000+i) = 0;
 		*(u32*)(0x06200000+i) = 0;
