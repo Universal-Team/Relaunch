cd97957079c783f8d33db13f5279dbadfdae80bc
FlameKat53 <38171335+FlameKat53@users.noreply.github.com>
2019-04-30 14:55:52 -0700

    Revert "Update main.h"
    This reverts commit af575858e09ec4dd07699d000a649fda528e88a6 [formerly 6a022a07b2bf3ff820c12bb873d7aa02fb1df0d2].
    
    
    Former-commit-id: a94ef184cb2d0a9a327b452707d9f399095b6c2c

--- menu/arm9/source/main.h
+++ menu/arm9/source/main.h
@@ -3,6 +3,9 @@
 
 #define POWERTEXT_DS	"POWER - Poweroff"
 #define POWERTEXT		"POWER - Reboot/[+held] Poweroff"
+#define POWERTEXT_3DS	"POWER - Sleep Mode screen"
+#define HOMETEXT 		"HOME - HOME Menu prompt"
+#define SCREENSHOTTEXT 	"R+L - Make a screenshot"
 
 extern char titleName[32];
 
@@ -12,6 +15,7 @@ extern bool appInited;
 
 extern bool arm7SCFGLocked;
 extern bool isRegularDS;
+extern bool is3DS;
 
 extern bool applaunch;
 
