ad1c455e1264849d189d8fbef678f0a230ce628c
FlameAlt53 <47005312+FlameAlt53@users.noreply.github.com>
2019-04-30 09:15:49 -0700

    Update stringtool.cpp

--- menu/arm9/source/common/stringtool.cpp
+++ menu/arm9/source/common/stringtool.cpp
@@ -15,7 +15,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
----------------------------------------------------------------------------------*/
+---------------------------------------------------------------------------------
 
 #include "stringtool.h"
 #include <cstdarg>
@@ -35,3 +35,4 @@ std::string formatString( const char* fmt, ... )
     va_end(argList);
     return str;
 }
+*/
