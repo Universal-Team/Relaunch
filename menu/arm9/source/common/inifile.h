8da35b74b4d33e5efc6f1a83df4310ba311b9e05
FlameKat53 <38171335+FlameKat53@users.noreply.github.com>
2019-04-30 14:55:52 -0700

    Revert "comment out this"
    This reverts commit c22fa4d05f5901c333f7f5dffb23f88d9339577e [formerly 25c1050b4c71cf5f02721d144ab9f4aad8498085].
    
    
    Former-commit-id: 779aff9bf019d825828d232497c1bb7b86dbdb01

--- menu/arm9/source/common/inifile.h
+++ menu/arm9/source/common/inifile.h
@@ -13,7 +13,7 @@
     GNU General Public License for more details.
     You should have received a copy of the GNU General Public License
     along with this program. If not, see <http://www.gnu.org/licenses/>.
-
+*/
 
 #ifndef _INIFILE_H_
 #define _INIFILE_H_
@@ -60,4 +60,4 @@ class CIniFile
     int GetInt(const std::string& Section,const std::string& Item);
 };
 
-#endif // _INIFILE_H_*/
+#endif // _INIFILE_H_
