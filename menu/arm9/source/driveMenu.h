31b3bc0452e30e92d0de7e49b61d60b29b92a411
FlameAlt53 <47005312+FlameAlt53@users.noreply.github.com>
2019-05-09 12:55:26 -0700

    part 1 of merge

--- menu/arm9/source/driveMenu.h
+++ menu/arm9/source/driveMenu.h
@@ -1,13 +1,24 @@
-#ifndef DRIVE_MENU_H
-#define DRIVE_MENU_H
+#ifndef MENU_H
+#define MENU_H
 
 #include <string>
 #include <vector>
 
+using namespace std;
+
+struct DirEntry {
+	string name;
+	off_t size;
+	bool isDirectory;
+	bool isApp;
+} ;
+
+std::string browseForFile (void);
+void getDirectoryContents (vector<DirEntry>& dirContents);
 extern bool flashcardMountSkipped;
 
 extern void driveMenu (void);
 
 
 
-#endif //DRIVE_MENU_H
+#endif //MENU_H
