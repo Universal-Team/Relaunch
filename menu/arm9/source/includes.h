d1002a5dbd209498d24b120b3b24c8ee948205fa
FlameAlt53 <47005312+FlameAlt53@users.noreply.github.com>
2019-06-06 09:48:13 -0700

    Create includes.h

---
+++ menu/arm9/source/includes.h
@@ -0,0 +1,18 @@
+#include <nds.h>
+#include <vector>
+#include <algorithm>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <dirent.h>
+#include <nds/arm9/dldi.h>
+#include <fat.h>
+#include <sys/stat.h>
+#include <sys/statvfs.h>
+
+#include "nds_loader_arm9.h"
+#include "inifile.h"
+#include "nitrofs.h"
+#include "main.h"
+#include "menu.h"
+#include "font.h"
