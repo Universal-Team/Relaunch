5715f26c8b6a7e5990aef0f0da12596478885d78
FlameKat53 <38171335+FlameKat53@users.noreply.github.com>
2019-07-29 20:34:02 -0500

    add some banner text code (unused for now)

--- menu/arm9/source/driveMenu.cpp
+++ menu/arm9/source/driveMenu.cpp
@@ -32,6 +32,9 @@ void dm_drawTopScreen(std::vector<DirEntry> dmItems, int startRow) {
 			//printf ("\x1b[42m  ");		// Print foreground green color
 			printf("  ");
 		}
+		/*for(int i=0;i<dmItems.size();i++) {
+    getIconTitle(dmItems[i].fullPath.c_str(), buffer, title);
+}*/
 		printf((dmItems[i + startRow].name.substr(0, SCREEN_COLS)).c_str());
 	}
 }
