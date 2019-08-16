c6af8ddbdf7c72c58de5f7e3def3d8e69a41d75d
FlameKat53 <38171335+FlameKat53@users.noreply.github.com>
2019-08-16 13:47:38 -0500

    Add files via upload

---
+++ menu/arm9/source/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define SCREEN_COLS 30
+#define ENTRIES_PER_SCREEN 19
+#define ENTRIES_START_ROW 3
+#define ENTRIES_START_ROW_EQ 3
+#define ENTRY_PAGE_LENGTH 10
+#define appVersion "\nRelaunch.nds v0.4"
+
+extern char titleName[32];
+
+extern int screenMode;
+
+extern bool appInited;
+
+extern bool arm7SCFGLocked;
+extern bool isRegularDS;
+
+extern void setFontTop();
+extern void setFontSub();
+
+#endif //MAIN_H
