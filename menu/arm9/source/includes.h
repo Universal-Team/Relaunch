812b400bddda93eab2fd74fdcb2fd1bb80506263
FlameKat53 <38171335+FlameKat53@users.noreply.github.com>
2019-09-04 11:26:24 -0500

    Update includes.h

--- menu/arm9/source/includes.h
+++ menu/arm9/source/includes.h
@@ -1,3 +1,10 @@
+/*-----------------------------------------------------------------
+ Not Copyright (ɔ) 2019
+	Evan "Flame" Rodgers
+	Ben "Epicpkmn11" Bogie
+	Rojelio "RocketRobz" Reyes
+	Kim "VoltZ" Perkovic
+------------------------------------------------------------------*/
 #ifndef _INCLUDES_H_
 #define _INCLUDES_H_
 
