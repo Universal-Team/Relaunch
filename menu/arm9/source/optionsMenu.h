ce2c4042dcac970a4abfb7871e3e7cc404205c97
FlameKat53 <38171335+FlameKat53@users.noreply.github.com>
2019-09-04 11:27:25 -0500

    Update optionsMenu.h

--- menu/arm9/source/optionsMenu.h
+++ menu/arm9/source/optionsMenu.h
@@ -1,3 +1,10 @@
+/*-----------------------------------------------------------------
+ Not Copyright (ɔ) 2019
+	Evan "Flame" Rodgers
+	Ben "Epicpkmn11" Bogie
+	Rojelio "RocketRobz" Reyes
+	Kim "VoltZ" Perkovic
+------------------------------------------------------------------*/
 #ifndef OPTIONS_MENU_H
 #define OPTIONS_MENU_H
 
