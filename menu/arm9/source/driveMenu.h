d20ec0a9f310d469abf236a70ecd08da6f825ee4
FlameKat53 <38171335+FlameKat53@users.noreply.github.com>
2019-09-04 11:23:55 -0500

    Update driveMenu.h

--- menu/arm9/source/driveMenu.h
+++ menu/arm9/source/driveMenu.h
@@ -1,3 +1,10 @@
+/*-----------------------------------------------------------------
+ Not Copyright (ɔ) 2019
+	Evan "Flame" Rodgers
+	Ben "Epicpkmn11" Bogie
+	Rojelio "RocketRobz" Reyes
+	Kim "VoltZ" Perkovic
+------------------------------------------------------------------*/
 #ifndef DRIVE_MENU_H
 #define DRIVE_MENU_H
 
