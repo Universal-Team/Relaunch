b4cfb2a7b7a831e202bb1f8fdf9bca9d961e61b6
FlameKat53 <38171335+FlameKat53@users.noreply.github.com>
2019-09-04 11:24:09 -0500

    Update fileBrowse.cpp

--- menu/arm9/source/fileBrowse.cpp
+++ menu/arm9/source/fileBrowse.cpp
@@ -1,3 +1,10 @@
+/*-----------------------------------------------------------------
+ Not Copyright (ɔ) 2019
+	Flame
+	Epicpkmn11
+	RocketRobz
+	VoltZ
+------------------------------------------------------------------*/
 #include "fileBrowse.h"
 #include <algorithm>
 #include <dirent.h>
