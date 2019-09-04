4276e539809f15e1e099d5cbb657011ab5a33d28
FlameKat53 <38171335+FlameKat53@users.noreply.github.com>
2019-09-04 11:26:07 -0500

    Update fileOperations.h

--- menu/arm9/source/fileOperations.h
+++ menu/arm9/source/fileOperations.h
@@ -1,3 +1,10 @@
+/*-----------------------------------------------------------------
+ Not Copyright (ɔ) 2019
+	Flame
+	Epicpkmn11
+	RocketRobz
+	VoltZ
+------------------------------------------------------------------*/
 #ifndef FILE_OPERATIONS_H
 #define FILE_OPERATIONS_H
 
