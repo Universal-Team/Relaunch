5cb1b84ec6b884e417e5147fb7534276e4291ba9
FlameKat53 <38171335+FlameKat53@users.noreply.github.com>
2019-10-14 12:45:53 -0500

    blah debug (#30)
    * Change build commit directory(?)
    
    * Update README.md
    
    * add A+X hotkey for new twlmenu autoboot...
    
    YSMenu broke A+B so theres now 2 of these menu hotkeys, the unlaunch style and the new "relaunch" style ;P
    
    * derp fix
    
    * Update optionsMenu.cpp

--- menu/arm9/source/optionsMenu.cpp
+++ menu/arm9/source/optionsMenu.cpp
@@ -142,6 +142,7 @@ void eqMenu (std::vector<DirEntry> ndsFiles) {
 		eqItems.push_back("D-PAD RIGHT");
 		eqItems.push_back("LOAD ERROR");
 		eqItems.push_back("BUTTON A+B (FILEMENU - FIXED)");
+		eqItems.push_back("BUTTON A+X (FILEMENU - FIXED)");
 		eqItems.push_back("SAVE & EXIT");
 	}
 
@@ -236,7 +237,7 @@ void eqMenu (std::vector<DirEntry> ndsFiles) {
 					}
 				}
 			} else {
-				if(eqCursorPosition == 16) {
+				if(eqCursorPosition == 17) {
 					ini.SaveIniFile("/_nds/Relaunch/Relaunch.ini");
 					screenMode = 0;
 					eqTextPrinted = false;
