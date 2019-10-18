1275ae1f489b895eaa7b8d0e42e2b6477188afe5
FlameKat53 <38171335+FlameKat53@users.noreply.github.com>
2019-10-18 13:16:26 -0500

    remove unused code

--- menu/arm9/source/fileBrowse.cpp
+++ menu/arm9/source/fileBrowse.cpp
@@ -11,20 +11,6 @@
 #include <nds.h>
 #include <unistd.h>
 
-off_t getFileSize(const char *fileName) {
-	FILE* fp = fopen(fileName, "rb");
-	off_t fsize = 0;
-	if (fp) {
-		fseek(fp, 0, SEEK_END);
-		fsize = ftell(fp);			// Get source file's size
-		fseek(fp, 0, SEEK_SET);
-	}
-	fclose(fp);
-
-	return fsize;
-}
-
-
 bool nameEndsWith(const std::string& name, const std::vector<std::string>& extensionList) {
 	if(name.substr(0, 2) == "._") return false;
 
@@ -47,63 +33,6 @@ bool dirEntryPredicate (const DirEntry& lhs, const DirEntry& rhs) {
 		return true;
 	}
 	return strcasecmp(lhs.name.c_str(), rhs.name.c_str()) < 0;
-}
-
-void getDirectoryContents (std::vector<DirEntry>& dirContents) {
-	std::vector<std::string> extensionList;
-	getDirectoryContents(dirContents, extensionList);
-}
-
-void getDirectoryContents (std::vector<DirEntry>& dirContents, std::vector<std::string>& extensionList) {
-	struct stat st;
-	dirContents.clear();
-
-	DIR *pdir = opendir (".");
-
-	if (pdir == NULL) {
-		iprintf ("Unable to open the directory.\n");
-	} else {
-
-		while(true) {
-			DirEntry dirEntry;
-
-			struct dirent* pent = readdir(pdir);
-			if(pent == NULL) break;
-
-			stat(pent->d_name, &st);
-			if (strcmp(pent->d_name, "..") != 0) {
-				dirEntry.name = pent->d_name;
-				dirEntry.isDirectory = (st.st_mode & S_IFDIR) ? true : false;
-				if (!dirEntry.isDirectory) {
-					dirEntry.size = getFileSize(dirEntry.name.c_str());
-				}
-				if((dirEntry.name.substr(dirEntry.name.find_last_of(".") + 1) == "nds")
-				|| (dirEntry.name.substr(dirEntry.name.find_last_of(".") + 1) == "NDS")
-				|| (dirEntry.name.substr(dirEntry.name.find_last_of(".") + 1) == "dsi")
-				|| (dirEntry.name.substr(dirEntry.name.find_last_of(".") + 1) == "DSI")
-				|| (dirEntry.name.substr(dirEntry.name.find_last_of(".") + 1) == "srl")
-				|| (dirEntry.name.substr(dirEntry.name.find_last_of(".") + 1) == "SRL"))
-				{
-					dirEntry.isApp = true;
-				} else {
-					dirEntry.isApp = false;
-				}
-
-				if (dirEntry.name.compare(".") != 0 && (dirEntry.isDirectory || nameEndsWith(dirEntry.name, extensionList))) {
-					dirContents.push_back (dirEntry);
-				}
-			}
-
-		}
-
-		closedir(pdir);
-	}
-
-	sort(dirContents.begin(), dirContents.end(), dirEntryPredicate);
-
-	DirEntry dirEntry;
-	dirEntry.isDirectory = true;
-	dirEntry.isApp = false;
 }
 
 void findNdsFiles(std::vector<DirEntry>& dirContents) {
